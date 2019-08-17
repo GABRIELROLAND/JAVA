782c0c525a6c14cac26572132d5cfe46049b2cf9
GABRIELROLAND <32078927+GABRIELROLAND@users.noreply.github.com>
2019-08-17 11:14:00 -0300

    Create Carroteste.java

---
+++ Carroteste.java
@@ -0,0 +1,26 @@
+package Exercicio;
+
+public class Carroteste {
+	
+	    public static void main(String args[]) {
+	        // 
+	        Carro a1;
+	        a1=new Carro();
+	        a1.modelo="Gol";
+	        a1.cor="Rosa Choque";
+	        a1.combustivel=23;
+	        a1.freio=true;
+	        
+	        
+	        // Metodos
+	        a1.Ligar();
+	        a1.Acelerar();
+	        a1.Freiar();
+	        a1.Desligar();
+	        
+	        
+	        
+	    }
+	}
+
+
